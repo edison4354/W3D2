ea59e01e1c435513bb7fe15c009305faa08d4898
Edison Li <47466335+edison4354@users.noreply.github.com>
2024-03-19 15:27:28 -0700

    AI attempt

--- practice-for-ch-ruby-memory-long-practice-main/computer_player.rb
+++ practice-for-ch-ruby-memory-long-practice-main/computer_player.rb
@@ -8,18 +8,40 @@ class ComputerPlayer
         @known_cards[value] << pos
     end
 
-    def receive_match[pos1, pos2]
-        @matched_cards << pos1
-        @matched_cards << pos2
+    def receive_match(pos1, pos2)
+        @matched_cards += [pos1, pos2]
     end
 
     def make_guess
         @known_cards.each do |k, v|
-            return v if v.length == 2      
+            return v[0] if v.length == 2
         end
         row = rand(0...4)
         col = rand(0...4)
-        return pos = row,col
+        pos = row,col
+
+        while @known_cards.values.flatten(1).include?(pos)
+            row = rand(0...4)
+            col = rand(0...4)
+            pos = row,col
+        end
+        return pos
+    end
+
+    def make_guess_2
+        @known_cards.each do |k, v|
+            return v[1] if v.length == 2
+        end
+        row = rand(0...4)
+        col = rand(0...4)
+        pos = row,col
+
+        while @known_cards.values.flatten(1).include?(pos)
+            row = rand(0...4)
+            col = rand(0...4)
+            pos = row,col
+        end
+        return pos
     end
 
 
--- practice-for-ch-ruby-memory-long-practice-main/game.rb
+++ practice-for-ch-ruby-memory-long-practice-main/game.rb
@@ -1,5 +1,6 @@
 require_relative 'board.rb'
 require_relative 'human_player.rb'
+require_relative 'computer_player.rb'
 
 class Game
     def initialize
@@ -8,6 +9,7 @@ class Game
     end
 
     def play
+        # computer = ComputerPlayer.new()
         already_guessed = []
         b = Board.new()
         b.populate
@@ -16,18 +18,20 @@ class Game
             @prev_guessed=0
             b.render
             @prev_guessed = HumanPlayer.new().make_guess
+            # @prev_guessed = computer.make_guess
             if !already_guessed.include?(b.grid[@prev_guessed[0]][@prev_guessed[1]])
                 b.reveal(@prev_guessed)
                 b.render
                 pos = HumanPlayer.new().make_guess
+                # pos = computer.make_guess_2
                 b.reveal(pos)
                 b.render
                 if b.grid[@prev_guessed[0]][@prev_guessed[1]] == b.grid[pos[0]][pos[1]]
                     b.grid[pos[0]][pos[1]].reveal
                     b.grid[@prev_guessed[0]][@prev_guessed[1]].reveal
                     already_guessed<<b.grid[pos[0]][pos[1]]
-
                 else
+                    # ComputerPlayer.new().receive_revealed_card(pos, b.grid[pos[0]][pos[1]])
                     b.grid[pos[0]][pos[1]].hide
                     b.grid[@prev_guessed[0]][@prev_guessed[1]].hide
                 end
